6fa02683c8ef8590e99079d36193d6960d9113b4
kpanchal99 <karanpanchal.git@gmail.com>
2024-05-31 15:32:09 +0530

    multi user connection

--- backend/Client.java
+++ backend/Client.java
@@ -2,26 +2,55 @@ package backend;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
-    Socket socket;
-    InputStreamReader inputStreamReader = null;
-    OutputStreamWriter outputStreamWriter = null;
-
-    BufferedReader bufferedReader = null;
-    BufferedWriter bufferedWriter = null;
 
     String clientUserName;
     int clientUserId;
 
+    Socket socket;
+    BufferedReader bufferedReader = null;
+    BufferedWriter bufferedWriter = null;
+
+
+
     public Client(String host, int PORT, String clientUserName, int clientUserId) throws IOException {
         this.clientUserName = clientUserName;
         this.clientUserId = clientUserId;
-        socket = new Socket(host,PORT);
+        socket = new Socket(host, PORT);
+        this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         System.out.println("Connected to " + socket.getInetAddress().getHostAddress() + ":" + socket.getPort());
     }
-    public static void main(String[] args) throws IOException {
-        Client client = new Client("localhost",9999, "KP",1);
 
+    public void sendMessageToServer() {
+        try {
+            bufferedWriter.write(clientUserName + "," + clientUserId);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+
+            Scanner scanner = new Scanner(System.in);
+            while (socket.isConnected()) {
+                String messageToSend = scanner.nextLine();
+                bufferedWriter.write(clientUserName + ": " + messageToSend);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        // to handle username and id exception
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your username: ");
+        String userName = scanner.nextLine();
+        System.out.println("Please enter your userId: ");
+        int userId = Integer.parseInt(scanner.nextLine());
+        Client client = new Client("localhost", 9999, userName, userId);
+        client.sendMessageToServer();
     }
 }
--- backend/Server.java
+++ backend/Server.java
@@ -3,35 +3,58 @@ package backend;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.http.HttpResponse;
+import java.util.Properties;
 
 public class Server {
 
     Socket socket = null;
     private ServerSocket serverSocket = null;
 
-    public Server(ServerSocket serverSocket){
+    BufferedReader bufferedReader = null;
+    BufferedWriter bufferedWriter = null;
+
+    public Server(ServerSocket serverSocket) {
         this.serverSocket = serverSocket;
     }
-    public void startServer(){
-        try{
-            if(!serverSocket.isClosed()){
+
+    public void startServer() {
+        try {
+            if (!serverSocket.isClosed()) {
                 System.out.println("Server has Started");
             }
-            while(!serverSocket.isClosed()){
+            while (!serverSocket.isClosed()) {
                 // client connection request
                 socket = serverSocket.accept();
                 System.out.println("New Client Connected");
+                ClientHandler clientHandler = new ClientHandler(socket);
+                Thread thread = new Thread(clientHandler);
+                thread.start();
 
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public static void main(String[] args) throws IOException {
 
-        ServerSocket serverSocket = new ServerSocket(9999);
+    public void listenClientMessage() {
+        while (!serverSocket.isClosed()) {
+            try {
+//                this.bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+                String clientMessage = this.bufferedReader.readLine();
+                System.out.println(clientMessage);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        // config data
+        Properties properties = new Properties();
+        int PORT = Integer.parseInt(properties.getProperty("server.port"));
+        ServerSocket serverSocket = new ServerSocket(PORT);
         Server server = new Server(serverSocket);
         server.startServer();
+        server.listenClientMessage();
     }
 }
